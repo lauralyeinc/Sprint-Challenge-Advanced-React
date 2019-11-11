d7d703e9a25389ef1ffc2f2a133f2b3c900e5b2b
Laura <theimer.laura@gmail.com>
2019-11-11 00:45:09 -0500

    mvp done

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,17 +1,42 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-import App from './App';
-import { render }  from "@test-library/react"; 
 
-test ('renders without crashing', () => {
-  render(<App />)
+import App from "./App.js"; 
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react'; 
+
+
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
+
+// test('App renders without crashing', () => {
+//   render(<App />); 
+// }); 
 
 test('should be true', () => {
   expect(true).toBe(true); 
-}); 
+});
 
-test('contains header and toggle', () => {
-  const { getByText } = render(<App />);
-  getByText(/ Womens World Cup Players/i);
-}); 
+test('it has "darkmodetogglesetup" in Nav.js in toggle set up', () => {
+  const container = render(<App />);
+  container.getByTestId("darkmodetogglesetup");
+})
+
+test('renders players state', async () => {
+  const { findByText } = await render(<App />);
+  findByText(/Ellen White/i);
+})
+
+
+
+
+
+
+
+
+
+
